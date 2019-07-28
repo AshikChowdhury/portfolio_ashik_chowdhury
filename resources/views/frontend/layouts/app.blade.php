00893e20c9e3d337688785252e824b2af40aac79
Ashik Chowdhury <ashikbracu@gmail.com>
2019-07-28 23:16:18 +0600

    background image change

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -44,25 +44,21 @@
 {{--    </body>--}}
 {{--</html>--}}
     <!DOCTYPE html>
-    <!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
-    <!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
-    <!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
-    <!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
-
     <head>
 
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
         <meta name="description" content="ashikchowdhury.xyz">
-        <meta name="author" content="Ashik Chowdhury">
+        <meta name="author" content="ASHIK CHOWDHURY">
 
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-        <title>Ashik</title>
+        <title>ASHIK CHOWDHURY</title>
 
         <!-- STYLES -->
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+
         {{ style(mix('css/frontend.css')) }}
 
         <!-- /STYLES -->
@@ -170,10 +166,10 @@
                                             <img src="{{asset('img/frontend/img/hero/img.jpg')}}" alt="" />
                                         </div>
                                         <div class="name_holder">
-                                            <h3>Alan <span>Michaelis</span></h3>
+                                            <h3>Ashik <span>Chowdhury</span></h3>
                                         </div>
                                         <div class="text_typing">
-                                            <p>I'm a <span class="arlo_tm_animation_text_word"></span></p>
+                                            <p>I'm a  <span class="arlo_tm_animation_text_word"></span></p>
                                         </div>
                                     </div>
                                 </div>
